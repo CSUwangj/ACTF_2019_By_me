4410b26e7d6f7da6e51417846b3dd7538b8a1145
CSUwangj <CSUwangj@protonmail.com>
2019-05-03 01:40:54 +0800

    feat: add regular challenge0

---
+++ Misc/regular_expression0/src/main.cpp
@@ -0,0 +1,139 @@
+#include <signal.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::regex;
+using std::regex_match;
+using std::string;
+using std::vector;
+
+vector<string> not_match_of_noo = {"consider", "accord", "concern", "commit", "approach", "utter conduct", "obtain", "policy", "stock", "property", "concept", "court appoint", "coast", "project", "commission", "constant", "constitute", "theory", "labor", "confer", "contract", "convince", "convention", "novel", "compel", "territory", "majority assert", "notion", "formal", "resource", "contempt", "tour", "mode", "distinction inclined", "oppress", "contend", "toil", "disposition", "boast advocate", "bestow", "notwithstanding", "lofty", "modest", "provoke", "contrive", "elaborate substantial", "frontier", "sob", "flourish", "ordain", "pious", "conspicuous", "retort", "bolt", "sanction", "proceeding", "throng", "exploit", "exertion", "endow", "imposed", "brook", "indication", "cower", "wont", "canon impel", "predecessor", "forsake", "philosophical", "grove frustrate", "illustrious", "pomp", "propriety", "consecrate", "proceeds", "fathom", "objective clad", "faction", "contrived", "manifestation", "insurgent rhetoric", "scrupulous", "imposing", "mortify", "comprehensive", "cohere", "recovering", "stratum conscientious", "conjure", "ominous", "foster", "admonish", "precision", "depose", "wanton", "odium", "nominal", "rave monetary", "headlong", "coax", "morbid", "pastoral", "dogged", "recourse", "constrained", "aversion", "conceit", "loath", "extort", "decorum", "enjoin", "disconcert", "horde", "foil", "projection prowess", "tout", "fortitude", "rout", "purport", "assiduous", "outskirts", "betrothed", "prospective", "advert peremptory", "ignominy", "ideology", "obtrude", "audacious", "construe ford", "fresco", "foreboding", "livelihood deign", "capricious", "stupendous", "ostensible", "propensity", "espouse", "erode", "canto docile", "patronize", "constituency", "practitioner ravel", "convalesce", "demoralize", "devolve", "unwonted", "uncouth", "extol", "knoll", "callous", "tenacious", "disconsolate", "imposition", "auspices sonorous", "exploitation", "ignominious", "onset", "conservatory", "voluble", "yeoman levity", "pinion", "axiom", "functionary", "philology", "propitious", "propitiate", "betoken"};
+
+vector<string> match_of_noo = {"minute", "evident", "practice", "intend", "issue", "establish", "engage", "scarce", "straight", "apparent", "fancy", "passage", "vain", "instance", "circumstances", "level affect", "institute", "render", "appeal", "generate", "range", "campaign", "league", "grant", "dwell entertain", "earnest", "yield", "wander", "insist", "knight", "inspire", "skill", "harry financial", "reflect", "furnish", "venture", "temper", "bent", "intimate", "undertake", "crew", "chamber", "humble", "scheme", "keen", "liberal", "despair", "tide", "attitude", "justify", "flag", "merit manifest", "scale", "persist", "plead", "weigh", "attribute", "exert", "stake", "perish", "rail", "cardinal", "allege", "multitude", "steep", "heed", "partial", "apt", "esteem credible", "tread", "ascertain", "fare", "cede", "perpetual", "decree", "derived", "facile", "cite", "warrant", "rider", "dense", "afflict", "vex", "gravity suspended", "jet", "assent", "purse", "plus", "exalt", "siege", "malice extravagant", "wax", "venerate", "assail", "sublime", "kindle", "humiliate suffrage", "ensue", "gale", "muse", "satire", "intrigue", "dispatch", "tract", "latitude", "vacate", "undertaking", "slay", "delicacy", "beseech", "device", "entreat", "impart", "partisan", "venerable", "restrained", "besiege", "rebuke", "ratify", "stump", "discreet", "wistful", "ripple", "premise", "subside", "adverse caprice", "muster", "accede", "fervent", "tribunal", "austere", "arbitrary", "exasperate", "edifice", "elude", "pervade", "repeal retiring", "incidental", "acquiesce", "slew", "usurp", "sentinel", "precept deference", "fray", "candid", "enduring", "impertinent", "bland", "insinuate", "suppliant", "languid", "infallible", "explicate", "gaunt", "ranging", "pacify", "ebb aide", "appease", "stipulate", "bate", "rampart", "tarry perpetrate", "luxuriant", "cant", "avarice", "edict", "symmetry", "capitulate", "arbitrate cleave", "append", "visage", "parable", "chastise", "veritable", "grapple", "gentry", "pall", "maxim", "dingy", "semblance", "asunder", "staid", "beguile", "deprave", "bequeath enigma", "vassal", "quail", "bulwark"};
+
+vector<string> match_of_power = {"x", "xx", "xxxx", "xxxxxxxx", "xxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"};
+
+vector<string> not_match_of_power = {"xxx", "xxxxx", "xxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"};
+
+vector<string> match_of_prime = {"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"};
+
+vector<string> not_match_of_prime = {"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"};
+
+enum {
+  ROUND1_LENGTH = 7,
+  ROUND2_LENGTH = 15,
+  ROUND3_LENGTH = 20,
+};
+
+void sigalarm(int sig) {
+  cout << endl
+       << "ALARM!" << endl;
+  exit(0);
+}
+
+void init() {
+  setbuf(stdin, 0);
+  setbuf(stdout, 0);
+  setbuf(stderr, 0);
+  signal(SIGALRM, sigalarm);
+  alarm(60);
+}
+
+bool challenge(const regex &input, const vector<string> &match, const vector<string> &not_match) {
+  std::smatch tmp;
+  vector<string> wrong_not_match;
+  vector<string> wrong_match;
+
+  for (auto &m : match) {
+    if (!regex_search(m, tmp, input)) wrong_not_match.push_back(m);
+  }
+
+  for (auto &m : not_match) {
+    if (regex_search(m, tmp, input)) wrong_match.push_back(m);
+  }
+
+  if (wrong_not_match.size() || wrong_match.size()) {
+    if (wrong_not_match.size()) {
+      cout << "You should match these but don't:" << endl;
+      for (auto &m : wrong_not_match) {
+        cout << m << endl;
+      }
+      cout << endl;
+    }
+    if (wrong_match.size()) {
+      cout << "You shouldn't match these but do match these:" << endl;
+      for (auto &m : wrong_match) {
+        cout << m << endl;
+      }
+      cout << endl;
+    }
+    cout << "Try again!" << endl
+         << "Your regex:";
+    return false;
+  }
+  return true;
+}
+
+void print_challenge(int length, const vector<string> &match, const vector<string> &not_match, string hint) {
+  cout << "Input a regex within " << length << " chars. Which should" << endl;
+  cout << "Match all of following:" << endl;
+  for (auto &m : match) {
+    cout << m << endl;
+  }
+  cout << endl
+       << "And none of following:" << endl;
+  for (auto &m : not_match) {
+    cout << m << endl;
+  }
+  cout << "HINT: " << hint << endl;
+}
+
+int main() {
+  init();
+  string input;
+  regex test;
+
+  print_challenge(ROUND1_LENGTH, match_of_noo, not_match_of_noo, "Noo hint");
+  cout << "Your regex:";
+  do {
+    cin >> input;
+    if (input.size() > ROUND1_LENGTH) {
+      cout << "Invalid Input!" << endl;
+      exit(0);
+    }
+    test = regex(input);
+  } while (!challenge(test, match_of_noo, not_match_of_noo));
+
+  print_challenge(ROUND2_LENGTH, match_of_prime, not_match_of_prime, "Prime");
+  cout << "Your regex:";
+  do {
+    cin >> input;
+    if (input.size() > ROUND2_LENGTH) {
+      cout << "Invalid Input!" << endl;
+      exit(0);
+    }
+    test = regex(input);
+  } while (!challenge(test, match_of_prime, not_match_of_prime));
+
+  print_challenge(ROUND3_LENGTH, match_of_power, not_match_of_power, "Power");
+  cout << "Your regex:";
+  do {
+    cin >> input;
+    if (input.size() > ROUND3_LENGTH) {
+      cout << "Invalid Input!" << endl;
+      exit(0);
+    }
+    test = regex(input);
+  } while (!challenge(test, match_of_power, not_match_of_power));
+
+  cout << "Isn't regex interesting?!" << endl
+       << "AURORA{regex_make_life_easier}" << endl;
+  return 0;
+}
